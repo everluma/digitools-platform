8f8cf4d2160473871db51a7fdbdb29da08e04fcc
everluma <farjanaaktarmonisha@gmail.com>
2026-04-01 23:41:41 +0600

    vite congig data remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,42 @@
-<h1>hello</h1>
+import Navbar from './components/Navbar';
+import Banner from './components/Banner'
+import Stats from './components/Stats'
+import MainSection from "./components/MainSection";
+import Steps from "./components/Steps";
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+import { useState } from 'react';
+
+import './App.css'
+
+
+function App() {
+
+
+
+  const [cart, setCart] = useState([])
+
+
+
+  return (
+
+    
+    <>
+    <Navbar cartCount={cart.length} />
+    
+     <div>
+      {/*Banner */}
+
+      <Banner />
+    </div>
+
+    <Stats/>
+    <MainSection cart={cart} setCart={setCart}/>
+    <Steps/>
+    <Pricing />
+    <Footer />
+    </>
+  )
+}
+
+export default App
